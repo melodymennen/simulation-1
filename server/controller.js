940ce29e39625ec36af163f01f0970bbc6d6a445
Melody <Melody@Melodys-MacBook.local>
2017-12-15 11:24:45 -0700

    set up server

---
+++ server/controller.js
@@ -0,0 +1,27 @@
+module.exports = {
+    createBin: (req,res,next) => {
+        const db = req.app.get('db');
+
+        
+    },
+    readShelves: (req,res,next) => {
+        const db = req.app.get('db');
+
+
+    },
+    readBins: (req,res,next) => {
+        const db = req.app.get('db');
+
+
+    },
+    updateBin: (req,res,next) => {
+        const db = req.app.get('db');
+
+
+    },
+    deleteBin: (req,res,next) => {
+        const db = req.app.get('db');
+
+
+    },
+}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+const controller = require('./controller');
+require('dotenv').config()
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = 3005
+// app.get('/api/shelf/:id', controller.readShelves)
+
+const port = process.env.PORT
 app.listen(port, () => console.log(`listening on port ${port}`));
