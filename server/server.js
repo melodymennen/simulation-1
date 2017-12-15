69da51921d49b40644ec4ef185b02bff9d261d95
Melody <Melody@Melodys-MacBook.local>
2017-12-15 09:56:29 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 3005
+app.listen(port, () => console.log(`listening on port ${port}`));
